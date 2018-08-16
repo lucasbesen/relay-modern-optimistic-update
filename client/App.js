0206a67ee57d1a0351a3a505fd45f1605022a13f
lucasbesen <lucassilvabesen@gmail.com>
2018-08-16 12:19:39 -0300

    Init app

---
+++ client/App.js
@@ -0,0 +1,60 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import {
+    Environment,
+    Network,
+    RecordSource,
+    Store,
+} from 'relay-runtime';
+
+import {
+  QueryRenderer,
+  graphql,
+} from 'react-relay';
+
+function fetchQuery(
+  operation,
+  variables,
+) {
+  return fetch('/graphql', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query: operation.text,
+      variables,
+    }),
+  }).then(response => {
+    return response.json();
+  });
+}
+
+const modernEnvironment = new Environment({
+  network: Network.create(fetchQuery),
+  store: new Store(new RecordSource()),
+});
+
+ReactDOM.render(
+  <QueryRenderer
+    environment={modernEnvironment}
+    query={graphql`
+      query AppQuery {
+        viewer {
+          ...OptimisticApp_viewer
+        }
+      }
+    `}
+    variables={{}}
+    render={({error, props}) => {
+      if (props) {
+        return <OptimisticApp viewer={props.viewer} />;
+      } else {
+        return <div>Loading</div>;
+      }
+    }}
+  />,
+  document.getElementById('root')
+);
