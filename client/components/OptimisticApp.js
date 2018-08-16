2f141c3b08e82d6493fce093912699b2d0e833ae
lucasbesen <lucassilvabesen@gmail.com>
2018-08-16 13:07:28 -0300

    Create OptimisticApp component

---
+++ client/components/OptimisticApp.js
@@ -0,0 +1,48 @@
+import TodoTextInput from './TodoTextInput';
+
+import React from 'react';
+import {
+  createFragmentContainer,
+  graphql,
+} from 'react-relay';
+
+class OptimisticApp extends React.Component {
+  _handleTextInputSave = (text) => {
+    AddTodoMutation.commit(
+      this.props.relay.environment,
+      text,
+      this.props.viewer,
+    );
+  };
+  render() {
+    const hasTodos = this.props.viewer.totalCount > 0;
+    return (
+      <div>
+        <section className="todoapp">
+          <header className="header">
+            <h1>
+              todos
+            </h1>
+            <TodoTextInput
+              autoFocus={true}
+              className="new-todo"
+              onSave={this._handleTextInputSave}
+              placeholder="What needs to be done?"
+            />
+          </header>
+          <List viewer={this.props.viewer} />
+        </section>
+      </div>
+    );
+  }
+}
+
+export default createFragmentContainer(OptimisticApp, {
+  viewer: graphql`
+    fragment OptimisticApp_viewer on User {
+      id,
+      totalCount,
+      ...TodoList_viewer,
+    }
+  `,
+});
